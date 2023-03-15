fb540b3e4e086fcf40f33ddcf8f370d08894bf47
Hariprasanth-HP <harireactjs@outlook.com>
2023-03-15 17:10:50 +0530

    auth changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,43 +1,44 @@
-const router = require("express").Router()
-const User = require("../model/User")
-const bcrypt = require("bcrypt")
+const router = require("express").Router();
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 // regsiter
 router.post("/register", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPass = await bcrypt.hash(req.body.password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
 
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
       password: hashedPass,
-    })
+    });
 
-    const user = await newUser.save()
-    res.status(200).json(user)
+    const user = await newUser.save();
+    res.status(200).json(user);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(`error:failed to register the user name ${error}`);
   }
-})
+});
 
 // login
 
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username })
+    const user = await User.findOne({ username: req.body.username });
     //if no user
-    !user && res.status(400).json("Wrong Credntials!")
+    !user && res.status(400).json("Wrong Credntials!");
 
     //if same user then compare password
-    const validate = await bcrypt.compare(req.body.password, user.password)
+    const validate = await bcrypt.compare(req.body.password, user.password);
     //if not validate
-    !validate && res.status(400).json("Wrong Credentials!")
+    !validate && res.status(400).json("Wrong Credentials!");
 
-    const { password, ...other } = user._doc
-    res.status(200).json(other)
+    const { password, ...other } = user._doc;
+    console.log("otherr", other);
+    res.status(200).json(other);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(`error:failed to login the user name ${error}`);
   }
-})
-module.exports = router
+});
+module.exports = router;
